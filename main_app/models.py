a33ae77d3848f146d8a3f0826c89d0a19ea60a74
pericarney00 <pericarney@gmail.com>
2025-09-15 16:09:08 -0500

    Models are created but not merged

---
+++ main_app/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+CATEGORIES = (
+  ('D', "Dairy"),
+  ('F', 'Frozen'),
+  ('R', 'Refrigerated'),
+  ('M&S', 'Meat & Seafood'),
+  ('P/DG', 'Pantry/Dry Goods'),
+  ('H', 'Houshold Items')
+)
+
+
+class Supplier(models.Model):
+  name = models.CharField(max_length=50)
+  phone = models.CharField(max_length=50)
+  email = models.EmailField()
+  address = models.CharField(max_length=80)
+  notes = models.TextField()
+
+  def __str__(self):
+    return self.name
+  
+  def get_absolute_url(self):
+    return reverse("product-detail", kwargs={"pk":self.id})
+  
+
+class Product(models.Model):
+  name = models.CharField(max_length=50)
+  brand = models.CharField(max_length=50)
+  description = models.TextField()
+  quantity = models.IntegerField()
+  category = models.CharField(
+    max_length= 1,
+    choices = CATEGORIES,
+    default = CATEGORIES[0][0]
+  )
+  image = models.ImageField()
+  date_added = models.DateField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  suppliers = models.ManyToManyField(Supplier)
+  def __str__(self):
+    return self.name
+  
+  def get_absolute_url(self):
+    return reverse('product-detail', kwargs={'pk':self.id })
+
